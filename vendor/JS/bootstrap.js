a67e36e6e3edec518396b79bae776e81912620d6
Brandon <brandsand@gmail.com>
2020-07-22 16:37:03 -0700

    minor updates

--- vendor/JS/bootstrap.js
+++ vendor/JS/bootstrap.js
@@ -1,10 +1,8 @@
 var script = document.createElement('script');
 var script2 = document.createElement('script');
-var script3 = document.createElement('script');
 
 script.type = 'text/javascript';
 script2.type = 'text/javascript';
-script3.type = 'text/javascript';
 
 script.src = "https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"; 
 script2.src = 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js';
@@ -16,8 +14,6 @@ script2.integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQR
 
 script.crossOrigin="anonymous";
 script2.crossOrigin="anonymous";
-script3.crossOrigin="anonymous";
 
 document.body.appendChild(script2);  
 document.body.appendChild(script);
-document.body.appendChild(script3);
